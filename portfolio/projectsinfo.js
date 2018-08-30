cef4e89cb9a49c885cd6d0f8afcf6306065db50b
Alejandro Mayol <alexmayol@hotmail.com>
2018-08-30 12:57:40 +0200

    image path fixed

--- portfolio/projectsinfo.js
+++ portfolio/projectsinfo.js
@@ -4,7 +4,7 @@ const projectsinfo = [
         "year": "2018",
         "excerpt": "Un framework CSS basado en CSS Variables y enfocado en utilidades para un rápido desarollo de interfaces.",
         "description": "name",
-        "image": "./img/remedy.png",
+        "image": "img/remedy.png",
         "stack": "CSS, Parcel",
         "link": "https://github.com/AlexMayol/remedy",
         "type": "app"
@@ -14,7 +14,7 @@ const projectsinfo = [
         "year": "2018",
         "excerpt": "Blog para la empresa Integratur, usando la API REST de WordPress y Vue para crear una SPA",
         "description": "name",
-        "image": "./img/blog.png",
+        "image": "img/blog.png",
         "stack": "Vue, WP API, Vue-router",
         "link": "https://blog.integratur.com",
         "type": "app"
@@ -24,7 +24,7 @@ const projectsinfo = [
         "year": "2016-2017",
         "excerpt": "Un diseño atractivo no implica un sitio web poco accesible. Esta demo se desarrolló para mostrar esa premisa.",
         "description": "name",
-        "image": "./img/hotel.png",
+        "image": "img/hotel.png",
         "stack": "HTML, CSS, JS, W3CSS",
         "link": "https://integratur.com/hotel/",
         "type": "web"
@@ -34,7 +34,7 @@ const projectsinfo = [
         "year": "2018",
         "excerpt": "Plataforma web que utiliza la API de Blogger para listar  y categorizar posts de recetas de cocina.",
         "description": "name",
-        "image": "./img/recipes.png",
+        "image": "img/recipes.png",
         "stack": "HTML, CSS, JS, Vue, Blogger",
         "link": "https://play.google.com/store/apps/details?id=com.tomateteam.healthmato",
         "type":"web"    
@@ -44,7 +44,7 @@ const projectsinfo = [
         "year": "2016",
         "excerpt": "Proyecto de fin de Grado. Portal que analiza la accesibilidad web de otra página web y detalla los resultados del análisis.",
         "description": "name",
-        "image": "./img/accestitan.png",
+        "image": "img/accestitan.png",
         "stack": "HTML, CSS, JS, PHP, PhantomJS, Heroku",
         "link": "https://accestitan.herokuapp.com/",
         "type":"web"    
@@ -54,7 +54,7 @@ const projectsinfo = [
         "year": "2018",
         "excerpt": "Clon de la página de portada del Centro Casaverde, para demostrar las distintas mejoras que se pueden aplicar en materia de accesibilidad.",
         "description": "name",
-        "image": "./img/casaverde.png",
+        "image": "img/casaverde.png",
         "stack": "HTML, CSS, Vue, Vue-Bootstrap",
         "link": "https://integratur.com/casaverde/",
         "type":"web"    
@@ -64,7 +64,7 @@ const projectsinfo = [
         "year": "2018",
         "excerpt": "Crea rápidamente alineaciones de fútbol y expórtalas como imagen.",
         "description": "name",
-        "image": "./img/soccer.png",
+        "image": "img/soccer.png",
         "stack": "name",
         "link": "name",
         "type":"app"    
@@ -74,7 +74,7 @@ const projectsinfo = [
         "year": "2017-2018",
         "excerpt": "Una característica de CSS tan importante merece un buen 'patio de juegos' donde experimentar con ella.",
         "description": "name",
-        "image": "./img/cssgrid.png",
+        "image": "img/cssgrid.png",
         "stack": "name",
         "link": "https://alexmayol.github.io/css-grid-playground/src/",
         "type":"web"    
@@ -84,7 +84,7 @@ const projectsinfo = [
         "year": "2018",
         "excerpt": "Simplifica la tarea de cambiar el tema de tu web con este componente de Vue",
         "description": "name",
-        "image": "./img/theme.png",
+        "image": "img/theme.png",
         "stack": "name",
         "link": "name",
         "type":"app"    
@@ -94,7 +94,7 @@ const projectsinfo = [
         "year": "2016",
         "excerpt": "Proyecto final para el itinerario de Videojuegos en el grado en Ingerniería Multimedia",
         "description": "name",
-        "image": "./img/mythia.png",
+        "image": "img/mythia.png",
         "stack": "OpenGL, C++, Irllciht, Unity, Box2D, Raknet",
         "link": "",
         "type":"game"    
@@ -104,7 +104,7 @@ const projectsinfo = [
         "year": "2013",
         "excerpt": "Videojuego para Android desarrollado como proyecto para la asignatura Sistemas Multimedia",
         "description": "name",
-        "image": "./img/healthmato.png",
+        "image": "img/healthmato.png",
         "stack": "Android, Java, Libgdx, Eclipse",
         "link": "https://play.google.com/store/apps/details?id=com.tomateteam.healthmato",
         "type":"game"    
@@ -114,7 +114,7 @@ const projectsinfo = [
         "year": "2016",
         "excerpt": "Videojuego para Android realizado durante la Global Game Jam 2016",
         "description": "name",
-        "image": "./img/walpurgis.png",
+        "image": "img/walpurgis.png",
         "stack": "Unity, C#, 3DS Max",
         "link": "https://play.google.com/store/apps/details?id=com.Pixys.WalpurgisRush",
         "type":"game"    
@@ -124,7 +124,7 @@ const projectsinfo = [
         "year": "2015",
         "excerpt": "Aplicación para Android desarrollada como proyecto para la asignatura Compresión y seguridad.",
         "description": "name",
-        "image": "./img/cifrador.png",
+        "image": "img/cifrador.png",
         "stack": "Android, Java, Eclipse",
         "link": "https://play.google.com/store/apps/details?id=cs.cifrador",
         "type":"app"    
@@ -134,7 +134,7 @@ const projectsinfo = [
         "year": "2014",
         "excerpt": "Videojuego para escritorio desarrollado como proyecto para la asignatura Fundamentos de los videojuegos",
         "description": "name",
-        "image": "./img/aztecalypse.png",
+        "image": "img/aztecalypse.png",
         "stack": "Android, Java, Libgdx, Eclipse",
         "link": "https://github.com/miquelgalianallorca/Aztecalypse",
         "type":"game"    
@@ -145,7 +145,7 @@ const projectsinfo = [
         "year": "2018",
         "excerpt": "Mejora del portal en cuanto a tiempo de carga y accesibilidad.",
         "description": "name",
-        "image": "./img/adaptamos.png",
+        "image": "img/adaptamos.png",
         "stack": "WordPress, CSS",
         "link": "http://www.adaptamosgroup.com/",
         "type":"web"    
@@ -155,7 +155,7 @@ const projectsinfo = [
         "year": "2014",
         "excerpt": "Práctica para la asigantura Programación Hipermedia I donde creamos un portal con registro y posibilidad de subir imágenes.",
         "description": "name",
-        "image": "./img/hotel.png",
+        "image": "img/hotel.png",
         "stack": "HTML, CSS, JS, PHP",
         "link": "https://github.com/AlexMayol/Stardust-Images",
         "type":"web"    
